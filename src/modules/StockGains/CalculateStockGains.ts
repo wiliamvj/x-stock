300b8259a74e8492d3858aecdf97a271e18cf891
wiliamvj <wiliamjoaquim@gmail.com>
2022-08-16 14:26:50 -0300

    end gains

--- src/modules/StockGains/CalculateStockGains.ts
+++ src/modules/StockGains/CalculateStockGains.ts
@@ -1,14 +1,41 @@
 import { Request, Response } from 'express';
+import moment from 'moment';
+
+import { searchStockQuote } from '../../utils/searchStockQuote';
+
+interface IGains {
+  stockName: string;
+  purchasedAt: string;
+  purchasedAmount: number;
+}
 
 class CalculateStockGains {
   async handle(req: Request, res: Response) {
+    const { stockName } = req.params as unknown as IGains;
+    const { purchasedAt, purchasedAmount } = req.query as unknown as IGains;
+
+    const priceOfToday = await searchStockQuote({
+      symbols: stockName,
+      date_from: moment().format('YYYY-MM-DD'),
+      date_to: moment().format('YYYY-MM-DD'),
+    });
+
+    const purchaseOfDate = await searchStockQuote({
+      symbols: stockName,
+      date_from: purchasedAt,
+      date_to: purchasedAt,
+    });
+
+    const gainsOfStock =
+      priceOfToday.data.data[0].open - purchaseOfDate.data.data[0].open;
+
     const result = {
-      name: 'ibm',
-      lastPrice: 199.9,
-      priceAtDate: 18.9,
-      purchasedAmount: 2,
-      purchasedAt: '2022-09-10',
-      capitalGains: 220.9,
+      name: stockName,
+      lastPrice: priceOfToday.data.data[0].open,
+      priceAtDate: purchaseOfDate.data.data[0].open,
+      purchasedAmount: purchasedAmount,
+      purchasedAt: purchasedAt,
+      capitalGains: gainsOfStock,
     };
 
     res.json(result);
