fffb0f8a4ceffbcae2706da5593401d3e726e361
wiliamvj <wiliamjoaquim@gmail.com>
2022-08-16 15:41:52 -0300

    finish test search stock quote

--- src/modules/StockQuote/tests/SearchStockQuote.spec.ts
+++ src/modules/StockQuote/tests/SearchStockQuote.spec.ts
@@ -1,9 +1,22 @@
 import { app } from '../../../app';
 import request from 'supertest';
 
+let token = '';
+
+beforeAll(async () => {
+  const res = await request(app).post('/create/token').send({
+    name: 'Wiliam V. Joaquim',
+    email: 'wiliamjoaquim@gmail.com',
+  });
+
+  token = res.body.token;
+});
+
 describe('Search today qupte', () => {
   it('I hope the user receives a quote today', async () => {
-    const res = await request(app).get(`/stock/ibm/quote`);
+    const res = await request(app)
+      .get(`/stock/ibm/quote`)
+      .set('Authorization', `Bearer ${token}`);
 
     expect(res.status).toBe(200);
     expect(res.body).toHaveProperty('name');
