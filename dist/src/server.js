8035d04c14027faf8e30343fc0ce70c9ae2392ed
wiliamvj <wiliamjoaquim@gmail.com>
2022-08-17 10:04:37 -0300

    init documatation with swagger

--- dist/src/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var app_1 = require("./app");
-app_1.app.listen(process.env.PORT || 3000, function () {
-    return console.log("\uD83D\uDD25 Server is running on port ".concat(process.env.PORT || 3000));
-});
-//# sourceMappingURL=server.js.map
